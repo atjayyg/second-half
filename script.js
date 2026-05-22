07fa11a04645766e5808803d21eec2139dd803e5
Jay <jaymatey2005@gmail.com>
2026-05-22 04:30:44 +0100

    build ch2 music player with spotify embeds and song navigation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const title = document.querySelector(".title");
 console.log(title);
 
-const words  = title.textContent.split(" ");
+const words = title.textContent.split(" ");
 console.log(words);
 
 title.textContent = "";
 
-for (let i = 0; i <words.length; i++){
+for (let i = 0; i < words.length; i++) {
     const span = document.createElement("span");
     span.classList.add("word");
     span.textContent = words[i];
@@ -16,16 +16,16 @@ for (let i = 0; i <words.length; i++){
 }
 
 const beginBtn = document.getElementById("begin-btn")
-beginBtn.addEventListener("click", function(){
+beginBtn.addEventListener("click", function () {
     document.getElementById("intro").classList.remove("active")
     document.getElementById("ch1").classList.add("active")
 })
 
-document.getElementById("gallery-btn").addEventListener("click", function(){
+document.getElementById("gallery-btn").addEventListener("click", function () {
     window.location.href = "gallery.html"
 })
 
-document.getElementById("ch2-prev-btn").addEventListener("click", function(){
+document.getElementById("ch2-prev-btn").addEventListener("click", function () {
     window.location.href = "gallery.html"
 })
 
@@ -35,18 +35,52 @@ const nextBtn = document.querySelectorAll(".next-btn")
 const prevBtn = document.querySelectorAll(".prev-btn")
 
 nextBtn.forEach(nextBtn => {
-    nextBtn.addEventListener("click", function() {
+    nextBtn.addEventListener("click", function () {
         document.querySelector(".screen.active").classList.remove("active")
         document.getElementById(nextBtn.dataset.next).classList.add("active")
     })
 });
 
 prevBtn.forEach(prevBtn => {
-    prevBtn.addEventListener("click", function(){
+    prevBtn.addEventListener("click", function () {
         document.querySelector(".screen.active").classList.remove("active")
         document.getElementById(prevBtn.dataset.prev).classList.add("active")
     })
 })
+
+const songCard = document.querySelectorAll(".song-card")
+let currentSongCard = 1
+document.getElementById("song-next").addEventListener("click", function () {
+    currentSongCard ++
+    currentSongCheck(currentSongCard)
+    document.querySelector(".song-card.active").classList.remove("active")
+    document.getElementById("ch2-song" + String(currentSongCard)).classList.add("active")
+})
+
+document.getElementById("song-prev").addEventListener("click", function() {
+    currentSongCard --
+    currentSongCheck(currentSongCard)
+    document.querySelector(".song-card.active").classList.remove("active")
+    document.getElementById("ch2-song" + String(currentSongCard)).classList.add("active")
+})
+
+function currentSongCheck(currentSongCard) {
+    if (currentSongCard == 4) {
+        document.getElementById("song-next").style.visibility = "hidden"
+    }
+    else {
+        document.getElementById("song-next").style.visibility = "visible"
+    }
+
+    if (currentSongCard == 1) {
+        document.getElementById("song-prev").style.visibility = "hidden"
+    }
+    else {
+        document.getElementById("song-prev").style.visibility = "visible"
+
+    }
+}
+
 
 if (window.location.hash === "#ch2") {
     document.getElementById("intro").classList.remove("active")
@@ -59,3 +93,5 @@ if (window.location.hash === "#ch1") {
     document.getElementById("ch1").classList.add("active")
     history.replaceState(null, "", " ")
 }
+
+currentSongCheck(currentSongCard)
